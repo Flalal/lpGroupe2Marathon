a51a81e5c486f3a23e3bba591ee0cfad21bd372f
florian.flahaut <florian-flahaut@hotmai.fr>
2017-12-20 11:52:10 +0100

    correction(twig Welcome) boucle for inutile

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,6 +6,8 @@ use App\Entity\Article;
 use App\Entity\Recipe;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcher;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -26,5 +28,13 @@ class ArticleController extends AbstractController
         return $this->render('article/show.html.twig',['articles'=>$articles]);
     }
 
-
+    /**
+     * @Route("/edit", name="app_article_edit")
+     */
+    public function edit(Request $request, EventDispatcher $eventDispatcher, Article $article)
+    {
+        $article = $this->getDoctrine()->getManager()->getRepository(Article::class)->find($article->getId());
+        $form = $this->createForm(ArticleType::class);
+        $form->handleRequest($request);
+    }
 }
